edec7ac4d1602cbe0f266b0c2bc6d8d4d36e2ed8
TargetXXX <128650940+TargetXXX@users.noreply.github.com>
2023-04-10 17:03:58 -0300

    Update config.php

--- FULLSTACK/config.php
+++ FULLSTACK/config.php
@@ -10,4 +10,10 @@
     //Conectando ao banco de dados.
 
     $con = new mySQLi(HOST, USER, PASS, BASE);
+    $sql = "CREATE TABLE IF NOT EXISTS niveis (Id int(11) AUTO_INCREMENT, Nivel varchar(100), PRIMARY KEY (Id))";
+    $sql2 = "CREATE TABLE IF NOT EXISTS desenvolvedores (Id int(11) AUTO_INCREMENT, Nivel_Id int(11), Nome varchar(100), DataNascimento date, Sexo char(2), Hobby varchar(100), PRIMARY KEY (Id), FOREIGN KEY (Nivel_Id) REFERENCES niveis(Id))";
+
+
+    mysqli_query($con, $sql);
+    mysqli_query($con, $sql2);
     
